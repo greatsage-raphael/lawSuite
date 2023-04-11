e26b3bef7a3280a9a6eea96924bc7ad4fc27146a
lubwama <bizzicole87@gmail.com>
2023-04-12 01:19:32 +0300

    V1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar: FC = () => {
   return (
     <div className="flex h-[60px] border-b border-gray-300 py-2 px-8 items-center justify-between">
       <div className="font-bold text-2xl flex items-center">
-        <div className="ml-2">LawSuite 💼</div>
+        <div className="ml-2">LawSuite v1 💼</div>
       </div>
       <div>
         <a
