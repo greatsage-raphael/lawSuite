c8272240a284eeb039902ddfc2b8abeca7dae5c8
lubwama <bizzicole87@gmail.com>
2023-04-15 18:24:18 +0300

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
+ 
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps<{}>) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
+      <Analytics mode={'production'} />;
     </main>
   );
 }
